ea1620f1ed8ae1cc9688f1d6b1501ff97bf09f35
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-05-14 14:20:07 +0500

    Add Files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import matplotlib
 matplotlib.use('Agg')  # Use non-interactive backend
 
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request
 import numpy as np
 import librosa
 import librosa.display
@@ -28,23 +28,30 @@ def upload():
         D = np.abs(librosa.stft(y))
         D_db = librosa.amplitude_to_db(D, ref=np.max)
         
-        colors = [(0, 'black'), (0.25, 'blue'), (0.5, 'green'), (0.75, 'yellow'), (1, 'red')]
+        # Define frequency-dependent colormap
+        num_colors = 256
+        colors = [(0, 'black')]
+        for i in range(1, num_colors):
+            frequency = i / num_colors * (sr / 2)  # Normalize frequency to the Nyquist frequency
+            hue = frequency / (sr / 2)  # Map frequency to hue
+            colors.append((i / (num_colors - 1), plt.cm.hsv(hue)[:3]))  # Add color tuple
+        
         cmap = mcolors.LinearSegmentedColormap.from_list("custom_colormap", colors)
         
         fig, ax = plt.subplots(figsize=(10, 4))
-        librosa.display.specshow(D_db, sr=sr, x_axis='time', y_axis='log', cmap=cmap, ax=ax)
-        ax.axis('off')  # Remove axes
-        img = io.BytesIO()
-        plt.savefig(img, format='png', bbox_inches='tight', pad_inches=0)  # Adjust padding
-        img.seek(0)
-        plot_url = base64.b64encode(img.getvalue()).decode()
+        img = librosa.display.specshow(D_db, sr=sr, x_axis='time', y_axis='log', cmap=cmap, ax=ax)
+        
+        # Add color bar
+        cbar = fig.colorbar(img, ax=ax, format='%+2.0f dB')
+        cbar.set_label('Decibels')
+        
+        # Save plot to bytes buffer
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)  # Adjust padding
+        buf.seek(0)
+        plot_url = base64.b64encode(buf.getvalue()).decode()
 
         return render_template('color_representation.html', plot_url=plot_url)
-
-@app.route('/color_representation')
-def color_representation():
-    plot_url = request.args.get('plot_url')
-    return render_template('color_representation.html', plot_url=plot_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
