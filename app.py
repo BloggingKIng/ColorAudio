3ac6af9556234072d02bdd9485e0f05532f78c73
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-06-09 23:03:39 +0500

    Minor change

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def split_image_into_chunks(image, chunk_size):
     return chunks
 
 def generate_color_palette():
-    chars = string.digits + string.ascii_uppercase + '!?., '
+    chars = string.digits + string.ascii_uppercase + '!?., \n'
     palette = {}
     
     num_colors = len(chars)
